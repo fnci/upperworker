7fe124da19a28ba91968ae839274949376ef9ddf
VF <vesselbit@gmail.com>
2023-04-14 22:06:37 -0400

    Add and connect to the MongoDB database called "ctrlchief"
    Create the necessary database to store places,
    users and related data in MongoDB.
    Add the "groundworks" collection to the "ctrlchief" database.
    Define the basic schema called Groundwork and make our model work.
    Tags: ES6 module syntax, groundworks, seedsDB

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 /* const express = require('express'); // CommonJS */
 import  express from 'express';
+import path from 'path';
 const app = express();
 const port = 3000; // Define a port to run the project.
 
-app.get("/", function(req, res) {
-    res.send("Welcome World!");
+import Groundwork from './models/groundwork.js';
+
+import mongoose from 'mongoose';
+const uri = 'mongodb+srv://vesselbit:0147@cluster-01.iozhzud.mongodb.net/ctrlchief';
+mongoose.connect( uri , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+/* db.on('error', err => { console.error.bind(console, "connection error: " + err.message) }); */
+db.on('error', console.error.bind(console, "connection error:"));
+db.once('open', () => {
+    console.log("Database connected");
+});
+
+
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.get("/", (req, res) => {
+    res.render('home');
+});
+
+app.get('/makegroundwork', async(req, res) => {
+    const ground = new Groundwork({
+        title: "New Ground",
+        description: "First work zone"
+    });
+    await ground.save();
+    res.send(ground);
 });
 
 app.listen(port);
---
+++ seeds/index.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import cities from '../seeds/cities.js';
+import { places, descriptors } from '../seeds/seedHelpers.js';
+import Groundwork from '../models/groundwork.js';
+
+const uri = 'mongodb+srv://vesselbit:0147@cluster-01.iozhzud.mongodb.net/ctrlchief';
+mongoose.connect( uri , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+/* db.on('error', err => { console.error.bind(console, "connection error: " + err.message) }); */
+db.on('error', console.error.bind(console, "connection error:"));
+db.once('open', () => {
+    console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Groundwork.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000)
+        const ground = new Groundwork({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${(sample(places))}`
+        })
+        await ground.save();
+    }
+    /* const c = new Groundwork({ title: 'purple field'});
+    await c.save(); */
+}
+seedDB().then(() => {
+    mongoose.connection.close();
+});
